5d6dfe0d5219a9f18613ecfd0ffd66400960f592
throwpedro <thordoor89@hotmail.com>
2019-12-08 15:41:38 +0100

    installed react-grid-system. lets try that.

--- src/shared/header/header.jsx
+++ src/shared/header/header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './header.scss';
+import { Container, Row, Col } from 'react-grid-system';
+
 
 const Header = () => {
     return (
-        <header className="pageHeader">
-        </header>
+        <Container className="header">
+            <Row>
+                <Col sm={6}>
+                One of three columns
+                </Col>
+                <Col sm={6}>
+                One of three columns
+                </Col>
+            </Row>
+        </Container>
      );
 }
 
