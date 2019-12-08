92d29659fb6352655856cb268d9baf1f2ed81a4e
throwpedro <thordoor89@hotmail.com>
2019-12-08 15:25:34 +0100

    Removed material ui.

--- src/shared/header/header.jsx
+++ src/shared/header/header.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './header.scss';
-import OutlinedInput from '@material-ui/core/TextField';
 
 const Header = () => {
     return (
         <header className="pageHeader">
-            <OutlinedInput
-                placeholder="Search"
-                color="primary"
-            />
         </header>
      );
 }
